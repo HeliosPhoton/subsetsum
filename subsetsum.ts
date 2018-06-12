9df36b95b0555de1070f3ed94974ce2fa8ef3796
Sunny Liu <sunnydangerliu@gmail.com>
2018-06-12 07:50:12 -0700

    first commit

---
+++ subsetsum.ts
@@ -0,0 +1,41 @@
+var numberList: number[] = [-7,-3,-2,8,5];
+var arrayLength: number;
+var subZeroBoolean: boolean = false;
+arrayLength = numberList.length;
+
+document.getElementById("numberList").innerHTML = numberList.toString();
+
+function subsetCalc(arr, arrayLength, sum)
+{
+    //key value pair
+    var subset = new Array();
+
+    //cumulative sum to see if we've checked the number already
+    var current_sum: number = 0;
+    for (var i = 0; i < arrayLength; i++)
+    {
+        current_sum = arr[i];
+        for (var j = i+1; j < arrayLength; j++)
+        {
+          if (current_sum == sum)
+          {
+            subZeroBoolean = true;
+            return;
+          }
+          current_sum += arr[j];
+          document.write(current_sum + "</br>");
+        }
+
+    }
+}
+
+subsetCalc(numberList,arrayLength,0);
+
+if (subZeroBoolean)
+{
+    document.getElementById("result").innerHTML = "Yes.";
+}
+else
+{
+    document.getElementById("result").innerHTML = "No.";
+}
